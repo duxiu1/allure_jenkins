6e564db4e6747db1046decbf7b79a1c7973fb41d
1149024259 <1149024259@qq.com>
2020-09-15 11:11:09 +0800

    1

---
+++ Scripts/test_01_allure.py
@@ -0,0 +1,19 @@
+import allure
+from selenium import webdriver
+
+
+class Test_01:
+
+    def setup_class(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://www.baidu.com")
+
+    @allure.step("第二步")
+    def abc(self):
+        print("111")
+
+    @allure.step("第一步")
+    def test_01(self):
+        self.abc()
+        allure.attach(self.driver.get_screenshot_as_png(), "截图", allure.attachment_type.PNG)
+        print("\n test_01")
